e7a50c8a316dcc6770b95cd5b52bbbf1e6ba6e59
petergemini <petergemini7764@gmail.com>
2026-01-12 20:03:00 +0300

    Fixed query script to handle multiple authors

--- LibraryProject/relationship_app/query_samples.py
+++ LibraryProject/relationship_app/query_samples.py
@@ -4,21 +4,24 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
-from relationship_app.models import Author, Library
+from relationship_app.models import Author, Book, Library, Librarian
 
 author_name = "Chinua Achebe"
-library_name = "Nairobi Public Library"
 
-author = Author.objects.get(name=author_name)
+author = Author.objects.filter(name=author_name).first()
+
 print(f"Books by {author_name}:")
-for book in author.books.all():
+for book in Book.objects.filter(author=author):
     print(f"- {book.title}")
 
-library = Library.objects.get(name=library_name)
+library_name = "Nairobi Public Library"
+library = Library.objects.filter(name=library_name).first()
+
 print(f"\nBooks in {library_name}:")
 for book in library.books.all():
     print(f"- {book.title}")
 
-librarian = library.librarian
 print(f"\nLibrarian for {library_name}:")
-print(f"- {librarian.name}")
+for librarian in Librarian.objects.filter(library=library):
+    print(f"- {librarian.name}")
+
