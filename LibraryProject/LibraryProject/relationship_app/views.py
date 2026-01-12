0bde22b7b8aa26473157cb620207bed33611bc8b
petergemini <petergemini7764@gmail.com>
2026-01-12 21:08:04 +0300

    Final commit before ALX submission

--- LibraryProject/LibraryProject/relationship_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Library
-
-def home(request):
-    libraries = Library.objects.all()
-    return render(request, "relationship_app/home.html", {"libraries": libraries})
-
